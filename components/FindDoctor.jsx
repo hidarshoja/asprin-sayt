4aaaf7020a0fb2d85a114322cefe86f0d3534cab
Heydar shoja <you@example.com>
2024-07-16 16:11:58 +0330

    bg-button

--- components/FindDoctor.jsx
+++ components/FindDoctor.jsx
@@ -2,6 +2,7 @@
 
 import React, { useState } from "react";
 import FindDoctorBox from "./FindDoctorBox";
+import { FaChevronDown } from "react-icons/fa";
 
 export default function FindDoctor() {
   const [activeIndex, setActiveIndex] = useState(4);
@@ -63,15 +64,15 @@ export default function FindDoctor() {
           <div className="w-full lg:w-1/2 bg-[#2171e8] rounded-2xl relative h-[325px]">
             <div className="w-full lg:absolute lg:top-[55px] lg:right-12 bg-white p-6 rounded-lg shadow-md">
               <div className="flex flex-col lg:flex-row items-center gap-1 justify-between mb-4">
-                <div className="w-full lg:w-1/2 flex items-center">
+                <div className="w-full bg-[#f1f1f1] rounded px-3 lg:w-1/2 flex items-center">
                 <img src="/search.svg" alt="search icon" />
                 <input
                   type="text"
                   placeholder="جستجوی شهر"
-                  className="w-full p-2 text-[12px] lg:text-[16px] outline-none rounded border-none"
+                  className="w-full bg-transparent p-2 text-[12px] lg:text-[16px] outline-none rounded border-none"
                 />
                 </div>
-                <select className="w-full lg:w-1/2 p-2 border rounded bg-transparent mr-2 text-[#9e9e9e] text-[12px] lg:text-[16px] border-none font-normal outline-none">
+                {/* <select className="w-full bg-[#f1f1f1] lg:w-1/2 p-2 border rounded  lg:mr-2 text-[#b7b6b6] text-[12px] lg:text-[16px] border-none font-normal outline-none">
                   <option value="" disabled selected>
                     جستجوی نوع بیماری
                   </option>
@@ -79,7 +80,24 @@ export default function FindDoctor() {
                   <option value="anemia">کم خونی</option>
                   <option value="diabetes">دیابت</option>
                   <option value="alzheimers">الزایمر</option>
-                </select>
+                </select> */}
+                 <div className="relative rounded-md bg-[#f1f1f1] w-full lg:w-1/2 ">
+              <FaChevronDown
+                className="absolute bg-transparent left-2 top-1/2 transform -translate-y-1/2 text-gray-400"
+                size={16}
+              />
+              <select
+                className="block appearance-none bg-transparent text-[12px] lg:text-[16px] w-full rounded-md  py-[9px] pl-8 pr-2   text-[#9E9E9E]  placeholder:text-gray-400"
+                defaultValue=""
+              >
+                <option value="" disabled>
+                  انتخاب شهر
+                </option>
+                <option value="specialty1">شهر اول</option>
+                <option value="specialty2">شهر دوم</option>
+                <option value="specialty3">شهر سوم</option>
+              </select>
+            </div>
               </div>
               <div className="grid grid-cols-3 gap-2">
                 {buttons.map((label, index) => (
